42fd9fd258eb86f4521a7124618ebd08d1779807
Stefan Nieuwenburg <82890714+Stefannieuwenburg@users.noreply.github.com>
2021-06-02 15:12:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+//const age = 16;
+//const isFemale = true;
+//const driverStatus = "bob";
+
+//opd 1
+if (age >= 18) {
+  console.log("ja je mag naar binnen");
+  document.write("ja je mag naar binnen");
+} else {
+  console.log("je wordt verzocht buiten te blijven");
+  document.write("je wordt verzocht buiten te blijven");
+}
+
+//opd 2
+if (isFemale) {
+  console.log("ja je bent een vrouw welkom ");
+  document.write("ja je bent een vrouw welkom");
+}
+else {
+  console.log("je bent een man, je mag niet naar binnen");
+  document.write("je bent een man, je mag niet naar binnen")
+}
+
+//opd3
+if (driverStatus === "bob") {
+  console.log("Je bent bob, je mag rijden");
+  document.write("Je bent bob, je mag rijden")
+} 
+else {
+  console.log("Je bent geen bob, je mag niet rijden");
+  document.write("Je bent geen bob, je mag niet rijden")
+}
